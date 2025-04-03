0434df57f0820f7a4d7825a561292f076d3abf41
fateme <ftm.amini2008@gmail.com>
2025-04-03 13:24:24 +0330

    sh_to_mi updated

--- date_converter.py
+++ date_converter.py
@@ -6,26 +6,35 @@ def shamsi_to_miladi(year, month, day):
         year += 622
     else:
         year += 621
+        
+   
     
     if is_leap(year):
         miladi_days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     else:
         miladi_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
-    shamsi_to_miladi = {
+    sh_to_mi = {
         1: (3, 21), 2: (4, 21), 3: (5, 22), 4: (6, 22), 5: (7, 23), 6: (8, 23),
         7: (9, 23), 8: (10, 23), 9: (11, 22), 10: (12, 22), 11: (1, 21), 12: (2, 20)
     }
     
-    miladi_month, base_day = shamsi_to_miladi[month]
+    miladi_month, base_day = sh_to_mi[month]
     miladi_day = base_day + (day-1)
     
     while miladi_day > miladi_days[miladi_month-1]:
         miladi_day -= miladi_days[miladi_month-1]
+        
         miladi_month += 1
         if miladi_month > 12:
             miladi_month = 1
-            year += 1
+    
+    if (month>=1 and month<=3)or (month>=6 and month<=10):
+        miladi_day-=1
+    if month==5 and day>=1 and day>=9:
+        miladi_day-=1
+    elif month==5 and day>=10:
+        miladi_day-=2
     
     print(f"Miladi: year:{year} month:{miladi_month} day:{miladi_day}")
 
@@ -41,21 +50,22 @@ def miladi_to_shamsi(year, month, day):
     else:
         shamsi_days = [31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29]
     
-    miladi_to_shamsi = {
+    mi_to_sha = {
         1: (10, 11), 2: (11, 12), 3: (12, 11), 4: (1, 12), 5: (2, 11), 6: (3, 11),
         7: (4, 10), 8: (5, 10), 9: (6, 10), 10: (7, 10), 11: (8, 10), 12: (9, 9)
     }
     
     # تبدیل روز میلادی به شمسی
-    shamsi_month, base_day = miladi_to_shamsi[month]
+    shamsi_month, base_day = mi_to_sha[month]
     shamsi_day = base_day + (day-1)
+    
     
     while shamsi_day > shamsi_days[shamsi_month - 1]:
         shamsi_day -= shamsi_days[shamsi_month - 1]
         shamsi_month += 1
+    
         if shamsi_month > 12:
             shamsi_month = 1
-            year += 1
     
     print(f"Shamsi: year:{year} month:{shamsi_month} day:{shamsi_day}")
 
@@ -70,4 +80,4 @@ while True:
     elif mode == 2:
         miladi_to_shamsi(year, month, day)
     else:
-        print("Invalid choice! Please enter 1 or 2.")
+        print("Invalid choice! Please enter 1 or 2.") 
